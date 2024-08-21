79a83e0a3b8fb1d814e6307968f4519d9ec3745d
Christopher Sackey <christopherniisakey18@gmail.com>
2024-08-21 15:35:34 +0000

    Nelson I am done, check out!

--- Day3/scripts.py
+++ Day3/scripts.py
@@ -1,5 +1,13 @@
 name = input('what is your name?')
 age = input('what is your age?')
 y = 'years' if int(age) > 1 else 'year'
-print('Heello ' + name + '! You are ' + age + ' ' + y + ' old.')
+print('Heello ' + ' ' + ' ' + name + ' ' + '! You are ' + age + ' ' + y + ' old.')
+# Message to nelson
+if int(age) < 12:
+    print('Hello' + ' '+ name + '! You are child' + ' of ' + age )
+elif 12 < int(age) < 18 :
+    print('Hello' + ' '+ name + '! You are a teenager' + ' ' + 'of' + ' ' + age + ' dont play with men')
+else:
+    print('Hello' + ' '+ name +'! You are an adult' + ' ' + 'of' + ' ' + age + ' ' + 'behave well')
+
 
